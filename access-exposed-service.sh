ea791e3c9ba5b9b54b3182a63241c938a548ddaa
malikgaurav217 <malikgaurav217@gmail.com>
2017-09-11 20:48:51 -0600

    Update access-exposed-service.sh

--- access-exposed-service.sh
+++ access-exposed-service.sh
@@ -1,7 +1,3 @@
-# create some pods under a deployment
-echo '** kubectl create -f ./my-nginx-exposed-service.yaml'
-kubectl create -f ./my-nginx-exposed-service.yaml
-
 # get service  my-nginx
 echo '** kubectl --namespace=development get svc my-nginx'
 kubectl --namespace=development get svc my-nginx
