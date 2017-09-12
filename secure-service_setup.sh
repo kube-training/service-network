4bf017b4eec53f9b86407208150f4c3fa0ea700d
malikgaurav217 <malikgaurav217@gmail.com>
2017-09-11 20:38:28 -0600

    Update secure-service_setup.sh

--- secure-service_setup.sh
+++ secure-service_setup.sh
@@ -1,12 +1,16 @@
 # create some pods under a deployment
-echo '** kubectl create -f ./my-nginx-service.yaml'
-kubectl create -f ./my-nginx-service.yaml
+echo '** kubectl create -f ./my-nginx-exposed-service.yaml'
+kubectl create -f ./my-nginx-exposed-service.yaml
 
 # get service  my-nginx
 echo '** kubectl --namespace=development get svc my-nginx'
 kubectl --namespace=development get svc my-nginx
 
-# describe service hostnames
+# describe service my-nginx
 echo '** kubectl --namespace=development describe svc my-nginx'
 kubectl --namespace=development describe svc my-nginx
+
+# get service  my-nginx
+echo '** kubectl --namespace=development get svc my-nginx'
+kubectl --namespace=development get svc my-nginx
 
