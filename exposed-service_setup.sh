21eec731e5cf258946f3af2e4ce938b6081d4b55
malikgaurav217 <malikgaurav217@gmail.com>
2017-09-11 20:46:59 -0600

    Update and rename secure-service_setup.sh to exposed-service_setup.sh

--- exposed-service_setup.sh
+++ exposed-service_setup.sh
@@ -13,4 +13,3 @@ kubectl --namespace=development describe svc my-nginx
 # get service  my-nginx
 echo '** kubectl --namespace=development get svc my-nginx'
 kubectl --namespace=development get svc my-nginx
-
