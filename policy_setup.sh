b68d093e57167a7a1c5ec55f04e22751a0769642
malikgaurav217 <malikgaurav217@gmail.com>
2017-09-11 20:19:22 -0600

    Delete policy_setup.sh

--- policy_setup.sh
+++
@@ -1,6 +0,0 @@
-# create my-nginx access policy
-echo '** kubectl create -f my-nginx-access-policy.yaml'
-kubectl create -f my-nginx-access-policy.yaml
-
-# List of policies
-kubectl --namespace=deplpyment get networkpolicies
