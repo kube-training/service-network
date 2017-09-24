b5d8ed4db45102e880c53f94e1db64657b5bb038
malikgaurav217 <malikgaurav217@gmail.com>
2017-09-24 12:47:06 -0600

    Update service_prod_setup.sh

--- service_prod_setup.sh
+++ service_prod_setup.sh
@@ -2,13 +2,13 @@
 echo '** kubectl create -f ./my-nginx-service.yaml'
 kubectl create -f ./my-nginx-service.yaml
 
-kubectl --namespace=development scale deployment my-nginx --replicas=0
-kubectl --namespace=development scale deployment my-nginx --replicas=2
+kubectl --namespace=production scale deployment my-nginx --replicas=0
+kubectl --namespace=production scale deployment my-nginx --replicas=2
 
 # get service  my-nginx
-echo '** kubectl --namespace=development get svc my-nginx'
-kubectl --namespace=development get svc my-nginx
+echo '** kubectl --namespace=production get svc my-nginx'
+kubectl --namespace=production get svc my-nginx
 
 # describe service hostnames
-echo '** kubectl --namespace=development describe svc my-nginx'
-kubectl --namespace=development describe svc my-nginx
+echo '** kubectl --namespace=production describe svc my-nginx'
+kubectl --namespace=production describe svc my-nginx
