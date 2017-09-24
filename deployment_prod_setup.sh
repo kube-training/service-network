f1e9ffca034c0e9fd4868bb7bb2648a634c98d1a
malikgaurav217 <malikgaurav217@gmail.com>
2017-09-24 12:44:27 -0600

    Create deployment_prod_setup.sh

---
+++ deployment_prod_setup.sh
@@ -0,0 +1,15 @@
+# create some pods under a deployment
+echo '-- kubectl create -f ./my-nginx-prod-deployment.yaml'
+kubectl create -f ./my-nginx-prod-deployment.yaml
+
+# List pods created with namespace production
+echo '-- kubectl --namespace=production get pods -l run=my-nginx -o wide'
+kubectl --namespace=production get pods -l run=my-nginx -o wide
+
+# List deployments
+echo '-- kubectl --namespace=production get deployment'
+kubectl --namespace=production get deployment
+
+# List pod IPs
+echo '-- kubectl --namespace=production get pods -l run=my-nginx -o yaml | grep podIP'
+kubectl --namespace=production get pods -l run=my-nginx -o yaml | grep podIP
