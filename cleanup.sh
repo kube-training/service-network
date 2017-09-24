4a5f87ecaed610a0ab3ffa0c5db845dea2d43979
malikgaurav217 <malikgaurav217@gmail.com>
2017-09-24 12:48:33 -0600

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -13,4 +13,20 @@ kubectl --namespace=development delete rc --all
 
 echo 'kubectl --namespace=development delete pods --all'
 kubectl --namespace=development delete pods --all
+
+# delete
+echo 'kubectl --namespace=production delete deployment --all'
+kubectl --namespace=production delete deployment --all
+
+echo 'kubectl --namespace=production delete rs --all'
+kubectl --namespace=production delete rs --all
+
+echo 'kubectl --namespace=production delete svc --all'
+kubectl --namespace=production delete services --all
+
+echo 'kubectl --namespace=production delete rc --all'
+kubectl --namespace=production delete rc --all
+
+echo 'kubectl --namespace=production delete pods --all'
+kubectl --namespace=production delete pods --all
 		
