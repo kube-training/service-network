dfeeb715404870532c9492d48a8ec1750c184edc
malikgaurav217 <malikgaurav217@gmail.com>
2017-09-24 12:38:13 -0600

    Update namespace_setup.sh

--- namespace_setup.sh
+++ namespace_setup.sh
@@ -8,7 +8,7 @@ kubectl apply -f ./development-namespace.yaml
 
 # kubectl create namespace
 echo 'kubectl apply -f ./prod-namespace.yaml'
-kubectl apply -f ./development-namespace.yaml
+kubectl apply -f ./prod-namespace.yaml
 
 # kubectl get namespaces
 echo 'kubectl get namespaces --show-labels'
